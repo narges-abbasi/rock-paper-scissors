a3add8da86c169e3738f6573ec0b7b10e418e9dd
Narges Abbasi <nargesabbasi@protonmail.com>
2025-10-29 10:31:14 +0100

    player objects passed

--- game.py
+++ game.py
@@ -3,31 +3,31 @@ from data import RULES
 
 class Game:
     # Game initialization and evaluation
-    def game_play(self) -> None:
-        user_score, computer_score, rounds, options, user_condition = self.game_setup()
+    def game_play(self, player1, player2) -> None:
+        p1_score, p2_score, rounds, options, end_condition = self.game_setup()
 
         while True:
-            user_choice = self.get_user_choice(options)
-            computer_choice = self.get_computer_choice(options)
+            p1_choice = player1.get_choice(options)
+            p2_choice = player2.get_choice(options)
 
-            print(f"You chose: {user_choice} \n Computer chose: {computer_choice}")
+            print(f"{player1.name} chose: {p1_choice}, {player2.name} chose: {p2_choice}")
 
-            user_score, computer_score, rounds = self.evaluate_round(
-                player_choice=user_choice,
-                computer_choice=computer_choice,
-                player_score=user_score,
-                computer_score=computer_score, rounds=rounds)
+            p1_score, p2_score, rounds = self.evaluate_round(
+                player1_choice=p1_choice,
+                player2_choice=p2_choice,
+                player1_score=p1_score,
+                player2_score=p2_score, rounds=rounds)
 
-            print(f"Your score: {user_score}\n Computer's score: {computer_score}")
+            print(f"Scores: {player1.name}: {p1_score}, {player2.name}: {p2_score}")
 
-            if self.check_end_condition(user_condition, rounds, user_score, computer_score):
+            if self.check_end_condition(end_condition, rounds, p1_score, p2_score):
                 break
 
-        self.game_end(user_score, computer_score)
+        self.game_end(p1_score, p2_score)
     
     def game_setup(self) -> tuple:
-        user_score = 0
-        computer_score = 0
+        p1_score = 0
+        p2_score= 0
         rounds = 0
 
         options = self.choose_version()
@@ -35,11 +35,11 @@ class Game:
         conditions = ["Best Out of Three Rounds", "First With Five Points"]
 
         while True:
-            user_condition = input(f"How should the game end? (choose (1) for: {conditions[0]}, (2) for: {conditions[1]})")
-            if user_condition in ["1", "2"]:
+            end_condition = input(f"How should the game end? (choose (1) for: {conditions[0]}, (2) for: {conditions[1]})")
+            if end_condition in ["1", "2"]:
                 break
             print("Invalid choice! Please enter 1 or 2")
-        return user_score, computer_score, rounds, options, user_condition
+        return p1_score, p2_score, rounds, options, end_condition
 
     # Classic vs Extended Versions
     def choose_version(self) -> list[str]:
@@ -53,32 +53,21 @@ class Game:
             else:
                 print("Invalid choice! Please enter 1 or 2")
 
-    def get_user_choice(self, options: list[str]) -> str:
-        while True:
-            user_choice = input(f"choose from {options}:").lower()
-            if user_choice not in options:
-                print("Invalid chioce! Please try again")
-                continue
-            return user_choice
-
-    def get_computer_choice(self, options: list[str]) -> str:
-        computer_choice = random.choice(options)
-        return computer_choice
 
     # Comparison and update score
-    def evaluate_round(self, player_choice: str, computer_choice: str, player_score: int, computer_score: int, rounds: int) -> tuple:
-        if player_choice == computer_choice:
+    def evaluate_round(self, player1_choice: str, player2_choice: str, player1_score: int, player2_score: int, rounds: int) -> tuple:
+        if player1_choice == player2_choice:
             print("It's a draw!")
-        elif computer_choice in RULES[player_choice]:
+        elif player2_choice in RULES[player1_choice]:
             print("You win this round!")
-            player_score += 1 
+            player1_score += 1 
             rounds += 1
         else:
             print("Computer wins this round!")
-            computer_score += 1
+            player2_score += 1
             rounds += 1
 
-        return player_score, computer_score, rounds 
+        return player1_score, player2_score, rounds 
     
     def check_end_condition(self, user_condition: str, rounds: int, user_score: int, computer_score: int) -> bool:
         if user_condition == "1":
@@ -89,12 +78,12 @@ class Game:
                 return True
         return False
 
-    def game_end(self, user_score: int, computer_score: int) -> None:
+    def game_end(self, player1_score: int, player2_score: int) -> None:
         print("-------------------------------------------------------------------")
         print("Game is Over!")
-        if user_score > computer_score:
-            print(f"You won the game ({user_score} - {computer_score})")
-        elif computer_score > user_score:
-            print(f"Computer won the game ({computer_score} - {user_score})")
+        if player1_score > player2_score:
+            print(f"You won the game ({player1_score} - {player2_score})")
+        elif player2_score > player1_score:
+            print(f"Computer won the game ({player2_score} - {player1_score})")
         else:
-            print(f"It's a draw!({computer_score} - {user_score})")
+            print(f"It's a draw!({player2_score} - {player1_score})")
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from game import Game
+from players import HumanPlayer, ComputerPlayer
 
 def main() -> None:
-    game = Game()
     print("Welcome to Rock, Paper, Scissors Game!")
-    game.game_play()
+
+    player1 = HumanPlayer("You")
+    player2 = ComputerPlayer("Computer")
+
+    game = Game()
+    game.game_play(player1=player1, player2=player2)
 
 if __name__ == "__main__":
     main()
